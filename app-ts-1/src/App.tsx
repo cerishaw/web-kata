fe2401d00715437bd681cda113fd01b4ab788a54
haletothewood <davidnhalewood@hotmail.com>
2018-05-12 12:22:34 +0100

    Submit kata

--- app-ts-1/src/App.tsx
+++ app-ts-1/src/App.tsx
@@ -1,20 +1,21 @@
 import * as React from 'react';
 import { Component } from 'react';
 import './App.css';
+import { GetData } from './data';
+import { Product } from './Models/Product';
+import ProductList from './ProductList';
 
 class App extends Component {
   render(): JSX.Element {
+    const data: Product[]  = GetData();
+
     return (
       <div className='App'>
         <div className='App-header'>
           <h2>Welcome Introduction to <code>web-kata#1-Typescript</code></h2>
         </div>
-        <p className='App-intro'>
-          To get started change this text and save save to reload.
-      </p>
-        <div className='products'>
-          ... your code here
-      </div>
+        <p className='App-intro'/>
+        <ProductList data={data} />
       </div>);
   }
 }
