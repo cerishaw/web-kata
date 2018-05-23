8e353e9e1791e2eb8a6ff42a20f5af0b60f8d8bd
Maya Malakova <maya.malakova@red-gate.com>
2018-05-23 14:40:40 +0100

    Toggle the description on clicking the +/-

--- app-ts-3/src/ProductComponent.tsx
+++ app-ts-3/src/ProductComponent.tsx
@@ -17,13 +17,22 @@ class ProductComponent extends Component<Props, ProductComponentState> {
   constructor(props: Props) {
     super(props);
     this.state = {showDescription: false};
+    this.handleToggleDescription = this.handleToggleDescription.bind(this);
   }
+
+    handleToggleDescription(event: React.MouseEvent<HTMLElement>): void {
+        let showDescription = this.state.showDescription;
+        this.setState({showDescription: !showDescription});
+    }
 
   render(): JSX.Element {
     return (
       <div className='product'>
         <div className='details'>
-          <div className='name'>{this.props.product.name}</div>
+          <div className='name'>{this.props.product.name}
+          <span onClick={this.handleToggleDescription}>{this.state.showDescription ? ' - ' : ' + '}</span>
+          </div>
+
             {this.state.showDescription ? <div className='desc'>{this.props.product.description}</div> : null}
         </div>
         <div className='actions'>
