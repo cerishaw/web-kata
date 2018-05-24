159fe71193ea6e312a1e87721e097eff208bfaba
Greg Smulko <greg.smulko@red-gate.com>
2018-05-24 14:34:47 +0100

    Add show/hide products

--- app-ts-3/src/ProductComponent.tsx
+++ app-ts-3/src/ProductComponent.tsx
@@ -8,19 +8,37 @@ interface Props {
   removeProduct: Function;
 }
 
-interface State { }
+interface State {
+  showDescription: boolean;
+}
 
 class ProductComponent extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
+    this.state = { showDescription: false };
+
+    this.toggleDesc = this.toggleDesc.bind(this);
+  }
+
+  toggleDesc() {
+    const prev = this.state.showDescription;
+    this.setState({showDescription: !prev});
   }
 
   render(): JSX.Element {
     return (
       <div className='product'>
         <div className='details'>
-          <div className='name'>{this.props.product.name}</div>
-          <div className='desc'>{this.props.product.description}</div>
+          <div className='name'>
+            {this.props.product.name}
+            <div
+              className='product-expand'
+              onClick={this.toggleDesc}
+            >
+              {this.state.showDescription ? '+' : '-'}
+            </div>
+          </div>
+          {this.state.showDescription ? <div className='desc'>{this.props.product.description}</div> : null}
         </div>
         <div className='actions'>
           <div className='remove' onClick={() => this.props.removeProduct(this.props.product)}>x</div>
