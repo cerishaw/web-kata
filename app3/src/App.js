05e11afa6787bd34d1423be66ea0a39a71cc0548
Ben Wood <benjaminlwood@gmail.com>
2018-07-12 09:51:47 +0100

    Completed kata 3

--- app3/src/App.js
+++ app3/src/App.js
@@ -1,58 +1,72 @@
 import _ from 'underscore'
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import './App.css'
 import data from './data.js'
 import Products from './Products.js'
 
 class App extends Component {
 
-  constructor(props){
-    super(props)
-    this.state= {products: data.products}
+    constructor(props) {
+        super(props)
+        this.state = {products: data.products, filter: ""}
 
-    this.handleAddProduct = this.handleAddProduct.bind(this)
-    this.removeProduct = this.removeProduct.bind(this)
-  }
+        this.handleAddProduct = this.handleAddProduct.bind(this)
+        this.removeProduct = this.removeProduct.bind(this)
+        this.handleFilterProducts = this.handleFilterProducts.bind(this)
+    }
 
-  handleAddProduct(event){
-    event.preventDefault()
-    const products = [...this.state.products]
+    handleAddProduct(event) {
+        event.preventDefault()
+        const products = [...this.state.products]
 
-    products.push({
-      name: event.target.name.value,
-      description: event.target.description.value
-    })
+        products.push({
+            name: event.target.name.value,
+            description: event.target.description.value
+        })
 
-    this.setState({products: products})
-  }
+        this.setState({products: products})
+    }
 
-  removeProduct(product){
-    const newProducts = _.filter(this.state.products, p => p.name !== product.name)
-    this.setState({products: newProducts})
-  }
+    removeProduct(product) {
+        const newProducts = _.filter(this.state.products, p => p.name !== product.name)
+        this.setState({products: newProducts})
+    }
 
-  render() {
-    return <div className="App">
-      <div className="App-header">
-        <h2>Kata 3- Filter, show and hide objects</h2>
-      </div>
-      <div className='filter-products'>Filter products here...</div>
-      <div className='add-product'>
-        <form onSubmit={this.handleAddProduct}>
-          <label>product name:
-            <input type='text' name='name' />
-          </label>
-          <label>description:
-            <input type='text' name='description'/>
-          </label>
-          <input type='submit' value='add product' />
-        </form>
-      </div>
-      <div className='products-container'>
-        <Products products={this.state.products} removeProduct={this.removeProduct} />
-      </div>
-    </div>
-  }
+    handleFilterProducts(event) {
+        event.preventDefault();
+        this.setState({
+            filter: event.target.value
+        });
+    }
+
+    render() {
+        return <div className="App">
+            <div className="App-header">
+                <h2>Kata 3- Filter, show and hide objects</h2>
+            </div>
+            <div className='filter-products'>
+                <form onChange={this.handleFilterProducts}>
+                    <label>Product name:
+                        <input type='text' name='name1'/>
+                    </label>
+                </form>
+            </div>
+            <div className='add-product'>
+                <form onSubmit={this.handleAddProduct}>
+                    <label>Product name:
+                        <input type='text' name='name'/>
+                    </label>
+                    <label>Description:
+                        <input type='text' name='description'/>
+                    </label>
+                    <input type='submit' value='add product'/>
+                </form>
+            </div>
+            <div className='products-container'>
+                <Products products={this.state.products.filter(product => product.name.toLowerCase().includes(this.state.filter.toLowerCase()))} removeProduct={this.removeProduct}/>
+            </div>
+        </div>
+    }
 }
 
 export default App
--- app3/src/Products.js
+++ app3/src/Products.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react'
 import './Products.css'
 
 class Product extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {showDescription: false};
+        this.toggleDescription = this.toggleDescription.bind(this)
+    }
+
+    toggleDescription() {
+        this.setState({showDescription: (!this.state.showDescription)});
+    }
+
     render(){
         return <div className='product'>
             <div className='details'>
                 <div className='name'>{this.props.product.name}</div>
-                <div className='desc'>{this.props.product.description}</div>
+                {this.state.showDescription ? <div className='desc'>{this.props.product.description}</div> : null}
             </div>
             <div className='actions'>
-                <div className='remove' title='fix me' onClick={() => this.props.removeProduct(this.props.product)}>x</div>
+                <span className='expand' onClick={this.toggleDescription}>{this.state.showDescription ? '−' : '+'}</span>
+                <span className='remove' onClick={() => this.props.removeProduct(this.props.product)}>×</span>
             </div>
         </div>
     }
