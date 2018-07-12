8d5bbd24d3e2fc1903766792ea3a5a6a98bc6fb2
Maya Malakova <maya.malakova@red-gate.com>
2018-07-12 14:23:03 +0100

    Can now add a product.

--- app-ts-5/app/src/App.tsx
+++ app-ts-5/app/src/App.tsx
@@ -12,17 +12,52 @@ interface Props {
 
 interface State {
     products: Product[];
+    newProductName: string;
+    newProductDescription: string;
 }
 
 class App extends Component<Props, State> {
     constructor(props: Props) {
         super(props);
-        this.state = {products: []};
+        this.state = {
+            products: [],
+            newProductName: '',
+            newProductDescription: ''
+        };
         fetch('/api/products/get').then(function (response: Response) {
             return response.json();
         }).then((json) => this.setState({products: json}));
 
         this.deleteProduct = this.deleteProduct.bind(this);
+        this.onNameChange = this.onNameChange.bind(this);
+        this.onDescriptionChange = this.onDescriptionChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onNameChange(event: React.FormEvent<HTMLInputElement>): void {
+        this.setState({
+            newProductName: event.currentTarget.value
+        });
+    }
+
+    onDescriptionChange(event: React.FormEvent<HTMLInputElement>): void {
+        this.setState({
+            newProductDescription: event.currentTarget.value
+        });
+    }
+
+    onSubmit(event: React.FormEvent<HTMLButtonElement>): void {
+        event.preventDefault();
+
+        let newProduct = {
+            name: this.state.newProductName,
+            description: this.state.newProductDescription
+        };
+
+        fetch('api/products/add', {method: 'POST', body: JSON.stringify(newProduct),  headers: {
+            'Content-Type': 'application/json'}
+        }).then(function (response: Response) { return response.json();
+        }).then(json => this.setState({products: json}));
     }
 
     deleteProduct(product: string) {
@@ -37,6 +72,27 @@ class App extends Component<Props, State> {
                 <div className='App-header'>
                     <h2>Kata 5 TypeScript - Interaction with backend server through REST API calls</h2>
                 </div>
+
+                <div className='add-product'>
+                    <form>
+                        <label>Product Name:
+                            <input
+                                name='newProductName'
+                                value={this.state.newProductName}
+                                onChange={this.onNameChange}
+                            />
+                        </label>
+                        <label>Product Description:
+                            <input
+                                name='newProductDescription'
+                                value={this.state.newProductDescription}
+                                onChange={this.onDescriptionChange}
+                            />
+                        </label>
+                        <button type='submit' onClick={this.onSubmit}>Submit</button>
+                    </form>
+                </div>
+
                 <div className='products-container'>
                     <ProductMenu products={this.state.products} deleteProduct={this.deleteProduct}/>
                     <Route
