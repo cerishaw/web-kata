6a1339e436da81de1515ab5056771e08f54ff985
Maya Malakova <maya.malakova@red-gate.com>
2018-07-12 14:10:27 +0100

    Delete products

--- app-ts-5/app/src/App.tsx
+++ app-ts-5/app/src/App.tsx
@@ -7,38 +7,47 @@ import ProductContainer from './ProductContainer';
 import { Product } from './Models/Product';
 import './App.css';
 
-interface Props { }
+interface Props {
+}
 
 interface State {
-  products: Product[];
+    products: Product[];
 }
 
 class App extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = { products: [] };
-    fetch('/api/products/get').then(function(response: Response) {
-        return response.json();
-    }).then((json) => this.setState({products: json}));
-  }
+    constructor(props: Props) {
+        super(props);
+        this.state = {products: []};
+        fetch('/api/products/get').then(function (response: Response) {
+            return response.json();
+        }).then((json) => this.setState({products: json}));
 
-  render(): JSX.Element {
-    return (
-      <div className='App'>
-        <div className='App-header'>
-          <h2>Kata 5 TypeScript - Interaction with backend server through REST API calls</h2>
-        </div>
-        <div className='products-container'>
-          <ProductMenu products={this.state.products} />
-          <Route
-            exact={true}
-            path='/products/:productName'
-            render={(props) => <ProductContainer {...props} products={this.state.products} />}
-          />
-        </div>
-      </div>
-    );
-  }
+        this.deleteProduct = this.deleteProduct.bind(this);
+    }
+
+    deleteProduct(product: string) {
+        fetch('/api/products/delete/' + product, {method: 'DELETE'}).then(function (response: Response) {
+            return response.json();
+        }).then((json) => this.setState({products: json}));
+    }
+
+    render(): JSX.Element {
+        return (
+            <div className='App'>
+                <div className='App-header'>
+                    <h2>Kata 5 TypeScript - Interaction with backend server through REST API calls</h2>
+                </div>
+                <div className='products-container'>
+                    <ProductMenu products={this.state.products} deleteProduct={this.deleteProduct}/>
+                    <Route
+                        exact={true}
+                        path='/products/:productName'
+                        render={(props) => <ProductContainer {...props} products={this.state.products}/>}
+                    />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- app-ts-5/app/src/ProductMenu.tsx
+++ app-ts-5/app/src/ProductMenu.tsx
@@ -6,6 +6,7 @@ import './ProductMenu.css';
 
 interface ProductMenuProps {
   products: Product[];
+  deleteProduct: Function;
 }
 
 class ProductMenu extends Component<ProductMenuProps, {}> {
@@ -13,7 +14,7 @@ class ProductMenu extends Component<ProductMenuProps, {}> {
     return (
       <div className='product-menu'>
         {this.props.products.map(
-          (p, i) => <ProductItem product={p} key={'product-' + i} />
+          (p, i) => <ProductItem product={p} key={'product-' + i} deleteProduct={this.props.deleteProduct} />
         )}
       </div>
     );
