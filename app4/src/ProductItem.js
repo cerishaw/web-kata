d372d9ddd12648358265606a39e3c295d7cd72d0
BLWood <benjaminlwood@googlemail.com>
2018-07-13 14:42:09 +0100

    Added hard links for each of the products

--- app4/src/ProductItem.js
+++ app4/src/ProductItem.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import './ProductItem.css'
 
 class ProductItem extends Component {
   render() {
     const name = this.props.product.name
     return <div className='product-item'>
-      <div className='name'>{name}</div>
+        <Link to={"/products/" + name.split(' ').join('')}>
+            <div className='name'>{name}</div>
+        </Link>
     </div>
   }
 }
