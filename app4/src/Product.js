29c0be89ac889a9a3acab936109601df8ec39ce4
BLWood <benjaminlwood@googlemail.com>
2018-07-13 16:41:49 +0100

    Added header tags to product name

--- app4/src/Product.js
+++ app4/src/Product.js
@@ -4,7 +4,7 @@ class Product extends Component {
   render() {
     return <div className='product'>
       <div className='details'>
-        <div className='name'>{this.props.product.name}</div>
+        <div className='name'><h1>{this.props.product.name}</h1></div>
         <div className='desc'>{this.props.product.description}</div>
       </div>
     </div>
