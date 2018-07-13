523b02390a1966488209aca9842bcb8ea3b8f211
BLWood <benjaminlwood@googlemail.com>
2018-07-13 16:40:26 +0100

    Finding the correct product and displaying it

--- app4/src/ProductContainer.js
+++ app4/src/ProductContainer.js
@@ -1,24 +1,27 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import {Redirect} from "react-router-dom";
 import _ from 'underscore'
 import data from './data.js'
 import './ProductContainer.css'
-import Product from './Product.js'
+import Product from "./Product";
 
 class ProductContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      products: data.products
+    constructor(props) {
+        super(props);
+        this.state = {
+            products: data.products
+        }
     }
-  }
 
-
-  render() {
-    console.log(this.props.match.params.product)
-    return <div className='product-container'>
-        {this.props.match.params.product}
-    </div>
-  }
+    render() {
+        let product = this.state.products.filter(product => product['name'].split(' ').join('') === this.props.match.params.product)[0];
+        if (!product) {
+            return <Redirect to="/"/>
+        }
+        return <div className='product-container'>
+            <Product product={product} />
+        </div>
+    }
 }
 
 export default ProductContainer
