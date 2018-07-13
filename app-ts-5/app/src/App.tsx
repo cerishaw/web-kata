75487dfe23762598163d8cf77c2e7869550933ec
Greg Smulko <greg.smulko@red-gate.com>
2018-07-13 14:53:17 +0100

    Add a new product

--- app-ts-5/app/src/App.tsx
+++ app-ts-5/app/src/App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Component } from 'react';
 import { Route } from 'react-router-dom';
 
-import { GetData } from './data';
+import { GetData, AddProduct } from './data';
 import ProductMenu from './ProductMenu';
 import ProductContainer from './ProductContainer';
 import { Product } from './Models/Product';
@@ -12,6 +12,7 @@ interface Props { }
 
 interface State {
   products: Product[];
+  newProduct: Product;
 }
 
 class App extends Component<Props, State> {
@@ -19,16 +20,38 @@ class App extends Component<Props, State> {
     super(props);
 
     // Access the REST API instead of grabbing products from data.ts
-    this.state = { products: [] };
+    this.state = { products: [], newProduct: { name: '', description: '' } };
     this.productDeleted = this.productDeleted.bind(this);
+    this.addProduct = this.addProduct.bind(this);
+    this.handleProductNameChange = this.handleProductNameChange.bind(this);
+    this.handleProductDescChange = this.handleProductDescChange.bind(this);
   }
 
   productDeleted(products: Product[]): void {
     this.setState({ products });
   }
 
+  addProduct(event: React.FormEvent<HTMLFormElement>): void {
+    event.preventDefault();
+    AddProduct(this.state.newProduct).then(products => this.setState({ products }));
+  }
+
   componentDidMount() {
     GetData().then(products => this.setState({ products }));
+  }
+
+  handleProductNameChange(event: React.FormEvent<HTMLInputElement>): void {
+    const productName = event.currentTarget.value;
+    let { newProduct } = this.state;
+    newProduct.name = productName;
+    this.setState({ newProduct: newProduct });
+  }
+
+  handleProductDescChange(event: React.FormEvent<HTMLInputElement>): void {
+    const productDescription = event.currentTarget.value;
+    let { newProduct } = this.state;
+    newProduct.description = productDescription;
+    this.setState({ newProduct: newProduct });
   }
 
   render(): JSX.Element {
@@ -36,6 +59,26 @@ class App extends Component<Props, State> {
       <div className='App'>
         <div className='App-header'>
           <h2>Kata 5 TypeScript - Interaction with backend server through REST API calls</h2>
+        </div>
+        <div>
+          <form onSubmit={this.addProduct}>
+            <label>
+              Product name:
+              <input
+                name='productName'
+                value={this.state.newProduct.name}
+                onChange={this.handleProductNameChange}
+              />
+            </label>
+            <label>Product description:
+              <input
+                name='productDescription'
+                value={this.state.newProduct.description}
+                onChange={this.handleProductDescChange}
+              />
+            </label>
+            <input type='submit' value='Add product' />
+          </form>
         </div>
         <div className='products-container'>
           <ProductMenu products={this.state.products} onProductDelete={this.productDeleted} />
--- app-ts-5/app/src/data.ts
+++ app-ts-5/app/src/data.ts
@@ -10,4 +10,15 @@ function RemoveProduct(product: Product): Promise<Product[]> {
     return fetch(`/api/products/delete/${name}`, { method: 'DELETE' }).then(data => data.json());
 }
 
-export { GetData, RemoveProduct };
+function AddProduct(product: Product): Promise<Product[]> {
+    return fetch(`/api/products/add`,
+                 {
+                     method: 'POST',
+                     body: JSON.stringify(product),
+                     headers: {
+                         'Content-Type': 'application/json'
+                     }
+                 }).then(data => data.json());
+}
+
+export { GetData, RemoveProduct, AddProduct };
